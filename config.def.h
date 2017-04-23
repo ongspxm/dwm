223e76f933142c6d63d85eed854b722aeda83e07
Metta Ong <ongspxm@gmail.com>
2017-04-23 21:44:58 +0800

    keyboard shortcuts
    - backlight
    - slock
    - firefox fixed in window 4

--- config.def.h
+++ config.def.h
@@ -28,6 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+    { "firefox", NULL, NULL, 1<<4, 0, -1 },
     { "trayer", NULL, NULL, 1<<8, 1, -1 },
 };
 
@@ -59,13 +60,19 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux" };
 static const char *slockcmd[] = { "slock" };
+static const char *blight1cmd[] = { "adj-blight.sh", "+" };
+static const char *blight2cmd[] = { "adj-blight.sh", "-" };
+static const char *slock[] = { "slock" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_F2,     spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_L,      spawn,          {.v = slockcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
+	{ MODKEY|ShiftMask,             XK_Down,   spawn,          {.v = blight2cmd } },
+	{ MODKEY|ShiftMask,             XK_Up,     spawn,          {.v = blight1cmd } },
+	{ MODKEY,                       XK_l,      spawn,          {.v = slock } },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_Tab,    focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
