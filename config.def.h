52426720fb41d99c18ed0d9a1f870f30ddf136fd
Metta Ong <ongspxm@gmail.com>
2017-05-02 21:57:55 +0800

    update browsers to default screen

--- config.def.h
+++ config.def.h
@@ -27,8 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "Vivaldi-stable", NULL, NULL, 1<<3, 0, -1 },
+    { "Firefox", NULL, NULL, 1<<3, 0, -1 },
+    { "Google-chrome", NULL, NULL, 1<<3, 0, -1 },
     { "trayer", NULL, NULL, 1<<8, 1, -1 },
 };
 
