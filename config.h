63d201c29d1ce391ba46019e288c81affc6d014f
ongspxm <ongspxm@gmail.com>
2025-05-08 17:57:36 +0000

    cached nix shell

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st","-e","nix-shell","--run","tmux" };
+static const char *termcmd[]  = { "st","-e","cached-nix-shell","--run","tmux" };
 static const char *slockcmd[] = { "slock",NULL };
 static const char *sound1cmd[] = { "adj-sound.sh", "+" };
 static const char *sound2cmd[] = { "adj-sound.sh", "-" };
